f2c21dc5d26a7ac872b968cad263a5264890fd77
tytypec <typecora@gmail.com>
2019-07-15 18:04:14 -0500

    Add files via upload

--- call.py
+++ call.py
@@ -2,18 +2,61 @@ print "enter your data priority: e=emergent r=routine "
 print "enter your data type: a=abdominal pain t=trauma s=sick person u=unknown problems"
 print "enter your data transport: f=finley m=mercy    "
 print "-----------------------------------------------"
-print "example e a m"
 
 priority, type, transport = raw_input ("example e a m: ").split()
 #read priority type transport
 
+#time stamp for time of call
+import time
+t = time.localtime()
+current_time = time.strftime("%H:%M", t)
+print(current_time)
+
+
+
+
+if type == "a":
+	chicom = "a patient with abdominal pain"
+elif type == "t":
+	chicom = "a patient suffering from a traumatic injury"
+elif type == "s":
+	chicom = "a sick person"
+else:
+	chicom = "an unknown problem"
+	
+#random number generation for the nurses name	
+from random import randrange
+random = (randrange(10))
+
+if random == 0:
+	nursename = " Kim "
+elif random == 1:
+	nursename = " Ashley "	
+elif random == 2:
+	nursename = " Ashton "
+elif random == 3:
+	nursename = " Alex "
+elif random == 4:
+	nursename = " Mary "
+elif random == 5:
+	nursename = " Taylor "
+elif random == 6:
+	nursename = " Kimberly "
+elif random == 7:
+	nursename = " Steph "
+elif random == 8:
+	nursename = " Olivia "
+else:
+	nursename = " Andy "
+	
+
 #priority- this section will have information regarding dispatch
 if priority == "e":
 	print "emergent"
-	priorityprime = "Dispatch: At 12:00 dispatch requested an emergent response from Medic 551 for a patient with abdominal pain. Medic 551 responded without incident or delay. Patient presented in a single family dwelling. "
+	priorityprime = "Dispatch: At " + current_time + " dispatch requested an emergent response from Medic 551 for " + chicom + ". Medic 551 responded without incident or delay. Patient presented in a single family dwelling. "
 else:
 	print "routine"
-	priorityprime = "Dispatch: At 12:00 dispatch requested a routine response from Medic 551 for $chicom. Medic 551 responded without incident or delay. Patient presented in a single family dwelling. "
+	priorityprime = "Dispatch: At " + current_time + " dispatch requested a routine response from Medic 551 for  " + chicom + ". Medic 551 responded without incident or delay. Patient presented in a single family dwelling. "
 	
 	
 	
@@ -41,13 +84,19 @@ else:
 
 if transport == "f":
 	print "finley"
-	transportprime = "Patient was transported routine to Finley hospital. Patient was moved from cot to bed using a sheet carry method. Patient care was transfered over to nurse"
+	transportprime = "Patient was transported routine to Finley hospital. Patient was moved from cot to bed using a sheet carry method. Patient care was transfered over to nurse" + nursename
 else:
 	print "mercy"
-	transportprime = "Patient was transported routine to Mercy hospital. Patient was moved from cot to bed using a sheet carry method. Patient care was transfered over to nurse "
+	transportprime = "Patient was transported routine to Mercy hospital. Patient was moved from cot to bed using a sheet carry method. Patient care was transfered over to nurse " + nursename
 
 #print (priorityprime)
 #print "(y)"
 #print "(z)"
 
-print priorityprime + typeprime + transportprime
+#print priorityprime + typeprime + transportprime
+
+finalprint = priorityprime + typeprime + transportprime
+
+print finalprint | xclip -selection clipboard
+#xclip -selection clipboard
+#print random
